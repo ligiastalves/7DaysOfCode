517109d260b4e943a049b0a03d24813058e0505e
Ligia Alves <ligia.stalves@gmail.com>
2022-08-07 20:33:13 -0300

    update

---
+++ day01/day01.js
@@ -0,0 +1,28 @@
+
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+    if (numeroUm == stringUm) {
+        document.write('As variáveis <strong>numeroUm</strong> e <strong>stringUm</strong> tem o mesmo valor, mas tipos diferentes' , "<br>")
+        }
+        else {
+        document.write('As variáveis <strong>numeroUm</strong> e <strong>stringUm</strong> não tem o mesmo valor' , "<br>")
+        }
+        
+        if (numeroTrinta === stringTrinta) {
+        document.write('As variáveis <strong>numeroTrinta</strong> e <strong>stringTrinta</strong> tem o mesmo valor e mesmo tipo', "<br>")
+        }
+        else {
+        document.write('As variáveis <strong>numeroTrinta</strong> e <strong>stringTrinta</strong> não tem o mesmo tipo' , "<br>")
+        }
+
+        if (numeroDez == stringDez) {
+        document.write('As variáveis <strong>numeroDez</strong> e <strong>stringDez</strong> tem o mesmo valor, mas tipos diferentes', "<br>")
+        }
+        else {
+        document.write('As variáveis <strong>numeroDez</strong> e <strong>stringDez</strong> não tem o mesmo valor', "<br>")
+        }
